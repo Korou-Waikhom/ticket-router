358db8a90428e561ab8d2a114ed3bba3a6a9c0c0
Korou Waikhom <korouwai101@gmail.com>
2026-09-02 11:06:36 +0530

    wiring the aiService to the main app

--- app.js
+++ app.js
@@ -11,12 +11,25 @@ app.post("/api/tickets", async (req, res) => {
   const { customer_email, original_text } = req.body;
 
   if (!customer_email || !original_text) {
-    return res.status(400).json({ error: "Email and text are required." });
+    return res.status(400).json({ error: "Email and text are required" });
   }
 
-  res
-    .status(202)
-    .json({ message: "Ticket received and processing setup is ready." });
+  try {
+    //  Send text and wait for the classification
+    console.log(`Processing ticket for ${customer_email}`);
+    const aiData = await classifyTicket(original_text);
+
+    console.log("AI Classification:", aiData);
+
+    res.status(200).json({
+      message: "Ticket successfully analyzed!",
+      ai_analysis: aiData,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal server error while processing ticket." });
+  }
 });
 
 app.listen(PORT, () => {
